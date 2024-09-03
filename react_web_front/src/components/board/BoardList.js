3062d186f2915966ad5e126a4811ef34f7e69ecd
Sujin <chosujin517@gmail.com>
2024-09-03 17:18:39 +0900

    5:20

--- react_web_front/src/components/board/BoardList.js
+++ react_web_front/src/components/board/BoardList.js
@@ -1,4 +1,21 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const BoardList = () => {
+  const backServer = process.env.REACT_APP_BACK_SERVKER;
+  const [boardList, setBoardList] = useState([]);
+  const [reqPage, setReqPage] = useState(1);
+
+  useEffect(() => {
+    axios
+      .get(`${backServer}/board/list/${reqPage}`)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [reqPage]);
   return (
     <section className="section board-list">
       <div className="page-title">자유게시판</div>
